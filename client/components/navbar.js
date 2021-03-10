f6fec468ccedfc81665c65217a6e0f7ab6c69cce
Alpay Aldemir <aldemir.alpay@gmail.com>
2021-03-10 13:00:41 -0500

    Styled navbar to establish common theme across logged-in/logged-out experience + centered coin-cell components within grid

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,84 +6,87 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <nav>
-    {isLoggedIn ? (
-      <section className="hero">
-        <div className="hero-head">
-          <div className="columns is-mobile is-marginless heading has-text-weight-bold">
-            <div className="column left">
-              {/* <figure className="navbar-item image has-text-black center mobile">
-                <i
-                  className="fas fa-bars"
-                  style={{width: '1.5rem', height: '1.5rem'}}
-                ></i>
-              </figure> */}
-              <a href="/trending" className="navbar-item">
-                <figure className="image">
-                  <img
-                    src="/logo-placeholder.png"
-                    style={{width: '6.25rem', height: '2rem'}}
-                  />
-                </figure>
-              </a>
+    <section className="is-light hero">
+      <div className="hero-head">
+        <div className="columns is-marginless heading has-text-weight-bold">
+          <div className="column left">
+            {/* burger-menu/drop down for mobile view */}
+
+            {/* burger-menu/drop down for mobile view */}
+            <figure className="navbar-item image desktop">
+              <img
+                src="/logo-placeholder.png"
+                style={{width: '6.25rem', height: '2rem'}}
+              />
+            </figure>
+            <Link
+              to="/trending"
+              className="navbar-item center has-text-black desktop"
+            >
+              <p>Trending Today</p>
+            </Link>
+            <Link
+              to="/coins/page/1"
+              className="navbar-item center has-text-black desktop"
+            >
+              <p>Top 100 Assets</p>
+            </Link>
+          </div>
+          {isLoggedIn ? (
+            <div className="column center">
+              <p>search bar goes here</p>
             </div>
-            <div className="column center buttons">
-              <Link to="/coins/page/1" className="navbar-item center button">
-                <p>Top 100 Assets</p>
-              </Link>
-            </div>
-            <div className="column right">
-              <Link to="/about" className="navbar-item right has-text-black">
-                <p>About Coinifer</p>
-              </Link>
+          ) : (
+            ''
+          )}
+          <div className="column right">
+            <Link
+              to="/about"
+              className="navbar-item right has-text-black desktop"
+            >
+              <p>About Coinifer</p>
+            </Link>
+            {isLoggedIn ? (
               <a
-                className="navbar-item right has-text-black"
+                className="navbar-item right has-text-black desktop"
                 href="#"
                 onClick={handleClick}
               >
                 Logout
               </a>
-              {/* <figure className="navbar-item image has-text-black center">
+            ) : (
+              <>
+                <Link
+                  to="/about"
+                  className="navbar-item has-text-black desktop"
+                >
+                  <p>About Coinifer</p>
+                </Link>
+                <Link
+                  to="/login"
+                  className="navbar-item has-text-black desktop"
+                >
+                  <p>Login</p>
+                </Link>
+                <Link
+                  to="/signup"
+                  className="navbar-item button is-primary desktop"
+                >
+                  <strong>Sign Up</strong>
+                </Link>
+              </>
+            )}
+            {/* <figure className="navbar-item image has-text-black center">
                 <i
                 className="fas fa-bars"
                 style={{width: '1rem', height: '1rem'}}
                 ></i>
               </figure> */}
-            </div>
           </div>
         </div>
-      </section>
-    ) : (
-      <section className="hero">
-        <div className="hero-head">
-          <div className="columns is-mobile is-marginless heading has-text-weight-bold">
-            <div className="column left">
-              <a href="/trending" className="navbar-item desktop">
-                <figure className="image">
-                  <img
-                    src="/logo-placeholder.png"
-                    style={{width: '6.25rem', height: '2rem'}}
-                  />
-                </figure>
-              </a>
-            </div>
-            <div className="column center buttons desktop">
-              <Link to="/coins/page/1" className="navbar-item center button">
-                <p>Top 100 Assets</p>
-              </Link>
-            </div>
-            <div className="column right buttons">
-              <Link to="/login" className="button is-light">
-                <p>Login</p>
-              </Link>
-              <Link to="/signup" className="button is-primary">
-                <strong>Sign Up</strong>
-              </Link>
-            </div>
-          </div>
-        </div>
-      </section>
-    )}
-    <hr />
+      </div>
+    </section>
+    <hr className="line-break" />
   </nav>
 )
 
